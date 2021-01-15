a6b6176d803a5d14c00e3bfefc35d28ee6f25148
Ashis Sharma <ashis@sukshi.com>
2021-01-15 15:06:03 +0530

    Minor Changes

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,20 @@ new fullpage('#fullpage',{
 
 document.getElementById('submit').addEventListener('click', async function(e) {
     e.preventDefault();
-    document.getElementById('button-img').style.animation = "plane-return 10s 1"; 
     let name = document.getElementById('name').value;
     let message = document.getElementById('message').value;
-    await emailjs.send('service_qrxfy7n','template_clgh61t', {name: name,message: message } ,'user_etepfJ0QfVa5RVKcZWOCb');
-    document.getElementById('name').value = "";
-    document.getElementById('message').value = "";
+    if(name === '' || message === '') {
+        document.getElementById('button-img').style.animation = "plane-return 10s 1"; 
+        setTimeout(() => {
+            alert("Please Enter all the details.")
+        }, 11000);
+    }
+    else {
+        document.getElementById('button-img').style.animation = "plane-return 10s 1"; 
+        await emailjs.send('service_qrxfy7n','template_clgh61t', {name: name,message: message } ,'user_etepfJ0QfVa5RVKcZWOCb');
+        document.getElementById('name').value = "";
+        document.getElementById('message').value = "";
+    }
 })
 
 new Typed('#typed',{
@@ -39,3 +47,15 @@ new Typed('#typed',{
     delaySpeed : 90,
     loop : true
 });
+
+let mediaFunc = window.matchMedia('(max-width: 600px)');
+
+if (mediaFunc.matches) {
+    document.getElementById('skills-col2').style.display = 'none';
+    const colSkills = ['GCP','Azure','Docker','Kubernetes','PWA','TensorFlow JS','Mongo DB'];
+    for(let i=0;i<colSkills.length;i++){
+        let li = document.createElement('li');
+        li.innerHTML = colSkills[i];
+        document.getElementById('skills-ul').appendChild(li);
+    }
+}
