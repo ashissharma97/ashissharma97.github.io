878d15d1e535b502c6ae96c35723b4da44bfc3f0
Ashis Sharma <ashis.sharma@sukshi.com>
2022-12-25 10:32:38 +0530

    Fix blogs sort by date

--- js/blogs.js
+++ js/blogs.js
@@ -10,8 +10,9 @@ var requestOptions = {
 (async function getMediumPosts(){
   const resp = await fetch("https://blogs-function.netlify.app/.netlify/functions/blogs-function", requestOptions)
   const json = await resp.json()
+  const sortedJson = json.sort((a,b) => Date.parse(b.addedOn) - Date.parse(a.addedOn))
   for(let i=0; i<json.length; i++){
-    document.getElementById('blogs-section').innerHTML += `<div class="card-container"> <a href="${json[i].link}"> <div class="card-image"><img src="${json[i].image}" alt="a brand new sports car" /> </div> <div class="card-body"><h1> ${json[i].title} </h1> </div> </a> </div>`
+    document.getElementById('blogs-section').innerHTML += `<div class="card-container"> <a href="${sortedJson[i].link}"> <div class="card-image"><img src="${sortedJson[i].image}" alt="a brand new sports car" /> </div> <div class="card-body"><h1> ${sortedJson[i].title} </h1> </div> </a> </div>`
   }
 })()
 
