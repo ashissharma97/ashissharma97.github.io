391c1dada25d0ad5ed490ad35c1fa8e700f486ec
Ashis Sharma <ashis@jukshio.com>
2024-02-23 17:47:58 +0530

    Updated profile image and content

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,92 @@
-new fullpage('#fullpage',{
-    anchors: ['landing', 'about', 'skills', 'experience', 'blogs', 'enquiry'],
-    verticalCentered: false,
-    onLeave: function(index,nextIndex,direction){
-        switch(nextIndex.index){
-            case 0:
-                document.title = "Ashis Sharma"
-                break;
-            case 1:
-                document.title = "About | Ashis Sharma"
-                break;
-            case 2:
-                document.title = "Skills | Ashis Sharma"
-                break;
-            case 3:
-                document.title = "Experiences | Ashis Sharma"
-                break;
-            case 4:
-                document.title = "Blogs | Ashis Sharma"
-                break;
-            case 5:
-                document.title = "Enquiry | Ashis Sharma"
-                break;
-            default:
-                document.title = "Ashis Sharma"
-                break;
-        }
+new fullpage("#fullpage", {
+  anchors: ["landing", "about", "skills", "experience", "blogs", "enquiry"],
+  verticalCentered: false,
+  onLeave: function (index, nextIndex, direction) {
+    switch (nextIndex.index) {
+      case 0:
+        document.title = "Ashis Sharma";
+        break;
+      case 1:
+        document.title = "About | Ashis Sharma";
+        break;
+      case 2:
+        document.title = "Skills | Ashis Sharma";
+        break;
+      case 3:
+        document.title = "Experiences | Ashis Sharma";
+        break;
+      case 4:
+        document.title = "Blogs | Ashis Sharma";
+        break;
+      case 5:
+        document.title = "Enquiry | Ashis Sharma";
+        break;
+      default:
+        document.title = "Ashis Sharma";
+        break;
     }
+  },
 });
 
-document.getElementById('submit').addEventListener('click', async function(e) {
-    e.preventDefault();
-    let name = document.getElementById('name').value;
-    let message = document.getElementById('message').value;
-    if(name === '' || message === '') {
-        alert("Please Enter all the details.")
-    }
-    else {
-        document.getElementById('button-img').style.animation = "plane 10s 1";
-        await emailjs.send('service_16ayyef','template_48nqu1e', {name: name,message: message } ,'TVJL_f4Vr5Uwg2l-R');
-        document.getElementById('name').value = "";
-        document.getElementById('message').value = "";
-    }
-})
-
-new Typed('#typed',{
-    strings : ['Engineer'],
-    typeSpeed : 80,
-    delaySpeed : 90,
-    loop : true
+document.getElementById("submit").addEventListener("click", async function (e) {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let message = document.getElementById("message").value;
+  if (name === "" || message === "") {
+    alert("Please Enter all the details.");
+  } else {
+    document.getElementById("button-img").style.animation = "plane 10s 1";
+    await emailjs.send(
+      "service_16ayyef",
+      "template_48nqu1e",
+      { name: name, message: message },
+      "TVJL_f4Vr5Uwg2l-R"
+    );
+    document.getElementById("name").value = "";
+    document.getElementById("message").value = "";
+  }
 });
 
-let mediaFunc = window.matchMedia('(max-width: 600px)');
+new Typed("#typed", {
+  strings: ["Software Engineer"],
+  typeSpeed: 80,
+  delaySpeed: 90,
+  loop: true,
+});
+
+let mediaFunc = window.matchMedia("(max-width: 600px)");
 
 if (mediaFunc.matches) {
-    document.getElementsByTagName('header')[0].style.display = 'none'
+  document.getElementsByTagName("header")[0].style.display = "none";
 }
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 var requestOptions = {
-    method: "get",
-    headers: myHeaders,
-    redirect: "follow",
+  method: "get",
+  headers: myHeaders,
+  redirect: "follow",
 };
 
-(async function getMediumPosts(){
-    const resp = await fetch("https://blogs-function.netlify.app/.netlify/functions/blogs-function", requestOptions)
-    const json = await resp.json()
-    const sortedJson = json.sort((a,b) => Date.parse(b.addedOn) - Date.parse(a.addedOn))
-    const len = window.matchMedia('(max-width: 600px)').matches ? 2 : 3;
-    for(let i=0; i<len; i++){
-      document.getElementById('index-blogs-section').innerHTML += `<div class="card-container"> <a href="${sortedJson[i].link}"> <div class="card-image"><img src="${sortedJson[i].image}" alt="a brand new sports car" /> </div> <div class="card-body"><h1> ${sortedJson[i].title} </h1> </div> </a> </div>`
-    }
-})()
+(async function getMediumPosts() {
+  const resp = await fetch(
+    "https://blogs-function.netlify.app/.netlify/functions/blogs-function",
+    requestOptions
+  );
+  const json = await resp.json();
+  const sortedJson = json.sort(
+    (a, b) => Date.parse(b.addedOn) - Date.parse(a.addedOn)
+  );
+  const len = window.matchMedia("(max-width: 600px)").matches ? 2 : 3;
+  for (let i = 0; i < len; i++) {
+    document.getElementById(
+      "index-blogs-section"
+    ).innerHTML += `<div class="card-container"> <a href="${sortedJson[i].link}"> <div class="card-image"><img src="${sortedJson[i].image}" alt="a brand new sports car" /> </div> <div class="card-body"><h1> ${sortedJson[i].title} </h1> </div> </a> </div>`;
+  }
+})();
 
 function goToBlogs() {
-    window.location.href = "/blogs.html";
+  window.location.href = "/blogs.html";
 }
 
-document.getElementById('all-blogs').onclick = goToBlogs;
+document.getElementById("all-blogs").onclick = goToBlogs;
